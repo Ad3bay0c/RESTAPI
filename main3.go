068e17a9267e5086868b65a55a8976d3adc92c08
tambarie <aclintonadebayo@gmail.com>
2021-09-09 15:30:50 +0100

    Server entry point

--- main3.go
+++ main3.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/Ad3bay0c/WebTesting/Api_Jwt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/login", Api_Jwt.LoginHandler).Methods("POST")
+	router.HandleFunc("/register", Api_Jwt.RegisterHandler).Methods("POST")
+	router.HandleFunc("/user", Api_Jwt.GetUser).Methods("GET")
+
+	log.Println("Server Started at Localhost:3000")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+}
