14513ba65b4e7a5f7c52a2236bb9e6d6f7540242
tambarie <aclintonadebayo@gmail.com>
2021-09-09 16:05:34 +0100

    Check if user exists

--- helpers/verification.go
+++ helpers/verification.go
@@ -1,1 +1,14 @@
 package helpers
+
+import "github.com/Ad3bay0c/WebTesting/db2"
+
+func CheckUser(username string) bool {
+	var name string
+	row := db2.DB.QueryRow("SELECT username FROM users WHERE username = $1", username)
+	_ = row.Scan(&name)
+	if name == "" {
+		return false
+	} else {
+		return true
+	}
+}
