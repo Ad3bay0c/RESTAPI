b8a50dc7e3a597cc96d2c7f73f06e8c4f15f78b3
tambarie <aclintonadebayo@gmail.com>
2021-09-09 15:29:35 +0100

    Hash Password Method for the AP

--- helpers/encrypt.go
+++ helpers/encrypt.go
@@ -1,1 +1,14 @@
 package helpers
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Bcrypt(pass string) (string, error) {
+	newPass, err := bcrypt.GenerateFromPassword([]byte(pass), 14)
+	if err != nil {
+		return "", errors.New("Error Hashing Pass")
+	}
+	return string(newPass), nil
+}
