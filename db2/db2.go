061815906f8a08e67397cfeca019ddce838766a8
tambarie <aclintonadebayo@gmail.com>
2021-09-09 15:28:49 +0100

    Database Connection Page for the API

--- db2/db2.go
+++ db2/db2.go
@@ -1,10 +1,10 @@
-package db
+package db2
 
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
@@ -14,8 +14,17 @@ const (
 	DB_PASS = "password"
 	DB_NAME = "ApiTesting"
 )
+var DB *sql.DB
 
-func ConnectDb() *sql.DB{
+func init() {
+	DB = connectDb()
+	if DB.Ping() != nil {
+		panic("Error Connecting to Database")
+	}
+	log.Printf("Database Connected")
+}
+
+func connectDb() *sql.DB{
 	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME))
 	if err != nil {
