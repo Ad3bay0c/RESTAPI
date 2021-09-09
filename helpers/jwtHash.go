0fff044687083490444792089cf5eab18c3b0412
tambarie <aclintonadebayo@gmail.com>
2021-09-09 15:48:23 +0100

    Create Token Added

--- helpers/jwtHash.go
+++ helpers/jwtHash.go
@@ -1,3 +1,19 @@
 package helpers
 
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
 
+func init () {
+	os.Setenv("SECRET_KEY", "mySecreteJWTKey")
+}
+func CreateToken(userId int64) (string, error)  {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Authorized": true,
+		"expiry": time.Now().Add(time.Minute * 30),
+		"userId": userId,
+	})
+	return token.SignedString(os.Getenv("SECRET_KEY"))
+}
