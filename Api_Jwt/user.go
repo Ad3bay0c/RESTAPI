8e4ad58328707de0d72177c522911c3de869fc12
tambarie <aclintonadebayo@gmail.com>
2021-09-09 20:16:38 +0100

    Get User Added

--- Api_Jwt/user.go
+++ Api_Jwt/user.go
@@ -2,10 +2,22 @@ package Api_Jwt
 
 import (
 	"encoding/json"
+	"github.com/Ad3bay0c/WebTesting/db2"
+	"log"
 	"net/http"
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"message":"Called"})
-	//fmt.Fprintf(w, "Get User Called")
+	id := r.Context().Value("userId")
+
+	var user User
+	row := db2.DB.QueryRow("SELECT id, username FROM users WHERE id = $1", id)
+	err := row.Scan(&user.ID, &user.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf(err.Error())
+		json.NewEncoder(w).Encode(Message{Message: "Server Error"})
+	}
+
+	json.NewEncoder(w).Encode(user)
 }
