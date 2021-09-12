5c278b2d7498697fc474eace8c57b46a302c83d6
tambarie <aclintonadebayo@gmail.com>
2021-09-12 22:26:16 +0100

    Main Added

--- main2.go
+++ main2.go
@@ -1,21 +1,22 @@
 package main
 
-import (
-	"github.com/Ad3bay0c/WebTesting/models"
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-)
-
 func main() {
-	router := mux.NewRouter()
+	//router := mux.NewRouter()
+	//
+	//router.HandleFunc("/person", models.CreatePersonEndpoint).Methods("POST")
+	//router.HandleFunc("/", models.GetPeopleEndPoint).Methods("GET")
+	//router.HandleFunc("/person/{id}", models.GetPersonEndPoint).Methods("GET")
+	//router.HandleFunc("/person/{id}", models.UpdatePersonEndpoint).Methods("PUT")
+	//router.HandleFunc("/person/{id}", models.DeletePersonEndPoint).Methods("DELETE")
+	//
+	//log.Printf("Server Started at localhost:1234")
+	//log.Fatal(http.ListenAndServe(":1234", router))
 
-	router.HandleFunc("/person", models.CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/", models.GetPeopleEndPoint).Methods("GET")
-	router.HandleFunc("/person/{id}", models.GetPersonEndPoint).Methods("GET")
-	router.HandleFunc("/person/{id}", models.UpdatePersonEndpoint).Methods("PUT")
-	router.HandleFunc("/person/{id}", models.DeletePersonEndPoint).Methods("DELETE")
-
-	log.Printf("Server Started at localhost:1234")
-	log.Fatal(http.ListenAndServe(":1234", router))
+	//os.Setenv("ACCESS_TOKEN", "SecretPassword")
+	//jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	//	"expiry" : time.Now().Add(time.Minute * 30).Unix(),
+	//	"user"	: 12345,
+	//})
+	//token, _ := jwtToken.SignedString([]byte(os.Getenv("ACCESS_KEY")))
+	//fmt.Println(token)
 }
